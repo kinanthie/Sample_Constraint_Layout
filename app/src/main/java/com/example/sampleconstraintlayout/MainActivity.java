e43d455f32c3e099a91e57e7526a2311d09b63be
KinanthieMPutrie <kinanthieputrie@gmail.com>
2022-03-03 20:51:32 +0700

    Membuat Event dengan menambahkan Toast pada button

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String nama, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.btSigIn);
+
+        edemail=findViewById(R.id.edEmail);
+
+        edpassword=findViewById(R.id.edPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                nama = edemail.getText().toString();
+
+                password = edpassword.getText().toString();
+
+                if (nama.equals("admin@mail.com") && password.equals("123")) {
+                    Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_LONG).show();
+                }else if (nama.equals(nama) && password.equals("123")) {
+                    Toast.makeText(getApplicationContext(), "Email yang anda masukkan salah", Toast.LENGTH_LONG).show();
+                }else if (nama.equals("admin@mail.com") && password.equals(password)) {
+                    Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_LONG).show();
+                }else if (nama.equals(nama) && password.equals(password)) {
+                    Toast.makeText(getApplicationContext(), "Email dan Password Salah", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
 }
